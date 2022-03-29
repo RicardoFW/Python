305b6ec22a0ec83eedfddc496b122c5bc129fd6b
Ricardo <ricardox15@hotmail.com>
2022-03-29 16:20:09 -0300

    Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ ex05.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um numero: "))
+#n1 = n+1
+#n2 = n-1
+#print("Analisando esse número {} Seu sucessor é {} e eu Antecessor é {}".format(n,n1,n2))
+
+print("Analisando esse número {} Seu sucessor é {} e eu Antecessor é {}".format(n,(n+1),(n-1)))
