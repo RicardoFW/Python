c189a407bcd405e29341f127dbdd7440510f8bf2
Ricardo <ricardox15@hotmail.com>
2022-03-29 15:19:04 -0300

    Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ ex03.py
@@ -0,0 +1,12 @@
+#Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+#n = int(input("Digite um valor: "))
+#n2= int(input("Digite Outro valor: "))
+#s = n + n2
+
+#print("As somas entre {} e {} e igual a {} ".format(n,n2,s))
+
+n = int(input("Digite um valor: "))
+n2 =int(input("Digite um outro valor: "))
+
+print("As somas entre {} e {} é: {} ".format(n,n2,n+n2))
